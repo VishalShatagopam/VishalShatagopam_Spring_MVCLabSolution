8d1114429626d4a39bb84e39957d37d80679cc82
Vishal <svishalkumar236@gmail.com>
2021-12-06 17:34:02 +0530

    Spring MVC Lab problem

---
+++ src/main/java/com/greatlearning/student/StudentService.java
@@ -0,0 +1,16 @@
+package com.greatlearning.student;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public interface StudentService{
+	public List<Student> findAll();
+
+	public Student findById(int theId);
+
+	public void save(Student student);
+
+	public void deleteById(int theId);
+
+}
